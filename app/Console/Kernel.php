6504d4d858c1e63e3fea4fcbf61bcfacc18527ba
井手 俊文 <toshifumiide198765@gmail.com>
2022-02-28 15:34:47 +0900

    task and scheduler

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,18 +4,61 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use App\Person;
+use App\Jobs\Myjob;
+use Illuminate\Support\Facades\Storage;
 
-class Kernel extends ConsoleKernel
-{
+
+class Kernel extends ConsoleKernel {
     /**
      * Define the application's command schedule.
      *
      * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
      * @return void
      */
-    protected function schedule(Schedule $schedule)
-    {
-        // $schedule->command('inspire')->hourly();
+    // protected function schedule(Schedule $schedule) {
+    // $schedule->command('inspire')->hourly();
+    // // 4-3 タスクとスケジューラ Scheduleクラスのコマンド実行
+    // $schedule->exec("./mycmd.sh");
+    // // これでプロジェクトのルートディレクトリに自作したmycmd.shを
+    // // $ php artisan schedule:runコマンドで実行できる様になった
+
+    // 4-3 タスクとスケジューラ Artisanコマンドを実行する「command」メソッド
+    // $schedule->command("queue:work --stop-when-empty");
+
+    // 4-3 タスクとスケジューラ クロージャーで実行する
+    // $count = Person::get()->count();
+    // $id = rand(0, $count) + 1;
+    // $schedule->call(function () use ($id) {
+    //     $person = Person::find($id);
+    //     MyJob::dispatch($person);
+    // });
+
+    // 4-3 invokeクラスの呼び出し
+    // $count = Person::all()->count();
+    // $id = rand(0, $count) + 1;
+    // $obj = new ScheduleObj($id);
+    // $schedule->call($obj);
+
+
+    // ディスパッチする場合
+    // $count = Person::all()->count();
+    // $id = rand(0, $count) + 1;
+    // $schedule->call(function () use ($id) {
+    //     MyJob::dispatch($id);
+    // });
+
+    // インスタンスを実行する場合
+    // $count = Person::all()->count();
+    // $id = rand(0, $count()) + 1;
+    // $schedule->call(new MyJob($id));
+    // }
+
+    // 4-3 ジョブメソッドによるジョブ実行
+    public function schedule(Schedule $schedule) {
+        $count = Person::get()->count();
+        $id = rand(0, $count) + 1;
+        $schedule->job(new MyJob($id));
     }
 
     /**
@@ -23,10 +66,22 @@ class Kernel extends ConsoleKernel
      *
      * @return void
      */
-    protected function commands()
-    {
-        $this->load(__DIR__.'/Commands');
-
+    protected function commands() {
+        $this->load(__DIR__ . '/Commands');
         require base_path('routes/console.php');
+    }
+}
+
+
+
+class ScheduleObj {
+    private $person;
+    public function __construct($id) {
+        $this->person = Person::find($id);
+    }
+    public function __invoke() {
+        Storage::append("person_access_log.txt", $this->person->all_data);
+        MyJob::dispatch($this->person);
+        return true;
     }
 }
--- app/Jobs/Myjob.php
+++ app/Jobs/Myjob.php
@@ -10,35 +10,59 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 // use App\Models\Person;
 use App\Person;
+use Illuminate\Support\Facades\Storage;
 
 class Myjob implements ShouldQueue {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
-
     protected $person;
+
+    public function getPersonId() {
+        return $this->person->id;
+    }
+
     /**
      * Create a new job instance.
      *
      * @return void
      */
-    public function __construct(Person $person) {
-        $this->person = $person;
+    public function __construct($id) {
+        // $this->person = $person;
+        $this->person = Person::find($id)->first();
     }
+
+    public function __invoke() {
+        $this->handle();
+    }
+
 
     /**
      * Execute the job.
      *
      * @return void
      */
+    // public function handle() {
+    //     //
+    //     $sufix = '[+MYJOB]';
+    //     if (strpos($this->person->name, $sufix)) {
+    //         $this->person->name = str_replace($sufix, "", $this->person->name);
+    //     } else {
+    //         $this->person->name .= $sufix;
+    //     }
+    //     // echo '<p class="myjob">THIS IS MY JOB FROM Jobs</p>';
+    //     $this->person->save();
+    // }
     public function handle() {
-        //
-        $sufix = '[+MYJOB]';
+        $this->doJob();
+    }
+    public function doJob() {
+        $sufix = "[MYJOB]";
         if (strpos($this->person->name, $sufix)) {
-            $this->person->name = str_replace($sufix, "", $this->person->name);
+            $this->person->name = str_repeat($sufix, "", $this->person->name);
         } else {
             $this->person->name .= $sufix;
         }
-        // echo '<p class="myjob">THIS IS MY JOB FROM Jobs</p>';
         $this->person->save();
+        Storage::append("person_access_log.txt", $this->person->all_data);
     }
 }
