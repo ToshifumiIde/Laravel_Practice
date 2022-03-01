099ac5f83f03284c056db58610549d542aaeeea5
井手 俊文 <toshifumiide198765@gmail.com>
2022-03-01 13:49:45 +0900

    use Vue in Laravel

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -13,63 +13,20 @@ use App\Events\PersonEvent;
 
 
 class HelloController extends Controller {
-    //
-    // public function index(Person $person) {
-    //     if ($person != null) {
-    //         $qname = $person->id % 2 == 0 ? "even" : "odd";
-    //         // MyJob::dispatch($person)->delay(now()->addMinutes(1));
-    //         MyJob::dispatch($person)->onQueue($qname);
-    //     }
-    //     $result = Person::get();
-    //     $msg = "this is message from HelloController index";
-    //     $data = [
-    //         "msg" => $msg,
-    //         "data" => $result,
-    //     ];
-    //     return view("hello.index", $data);
-    // }
-    // public function post(Request $request) {
-    //     $msg = "message from HelloController post";
-    //     $id = $request->id;
-    //     $result = Person::get();
-    //     $data = [
-    //         "msg" => $msg,
-    //         "id" => $id,
-    //         "data" => $result,
-    //     ];
-    //     return view("hello.index", $data);
-    // }
+    // 5-1 Vue.jsの利用
+
     public function index() {
-        $msg = "this is message from HelloController index";
-        $result = Person::get();
+
         $data = [
-            "input" => "",
-            "msg" => $msg,
-            "data" => $result,
+            "msg" => "This is Vue.js application",
         ];
         return view("hello.index", $data);
     }
-    // public function send(Request $request) {
-    //     $id = $request->id;
-    //     $person = Person::find($id);
-    //     // dd($person);
-    //     dispatch(function () use ($person) {
-    //         Storage::append('person_access_log.txt', $person->all_data);
-    //         dd($person);
-    //     });
-    //     return redirect()->route("hello");
-    // }
-
-    // 4-2 Event
-    public function send(Request $request) {
-        $id = $request->id;
-        $person = Person::find($id);
-        event(new PersonEvent($person));
-        $data = [
-            "input" => "",
-            "msg" => 'id=' . $id,
-            "data" => [$person],
-        ];
-        return view("hello.index", $data);
+    public function json($id = -1) {
+        if ($id == -1) {
+            return Person::get()->toJson();
+        } else {
+            return Person::find($id)->toJson();
+        }
     }
 }
--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+{{-- <!DOCTYPE html>
 <html lang="ja">
 <head>
     <meta charset="UTF-8">
@@ -36,5 +36,26 @@
         @endforeach
     </table>
 </div>
+</body>
+</html> --}}
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <link rel="stylesheet" href="{{mix('css/app.css')}}">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body style="padding:10px;">
+
+<h1>Hello/Index</h1>
+<p>{{$msg}}</p>
+<div id="app">
+    <example-component></example-component>
+    <my-component></my-component>
+</div>
+    <script src="{{mix('js/app.js')}}"></script>
 </body>
 </html>
